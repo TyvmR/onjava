066ff70052d4be2192154ce2e4d16df4e66e7c76
john.huang <john.huang@cloudwise.com>
2022-07-13 10:30:15 +0800

    add msg

---
+++ basic/src/main/java/org/example/chapter11/StateChange.java
@@ -0,0 +1,32 @@
+package org.example.chapter11;
+
+public class StateChange {
+
+    int i  = 0 ;
+
+    public void increatement(){
+        i++;
+    }
+
+
+    class InnerStateChange{
+
+        public void print(){
+            System.out.println(i);
+        }
+    }
+
+
+    //内部类与共享外部类状态
+    public static void main(String[] args) {
+        StateChange stateChange = new StateChange();
+        StateChange.InnerStateChange innerStateChange = stateChange.new InnerStateChange();
+        innerStateChange.print();
+        stateChange.increatement();
+        innerStateChange.print();
+        InnerStateChange innerStateChange1 = stateChange.new InnerStateChange();
+        innerStateChange1.print();
+    }
+
+
+}
