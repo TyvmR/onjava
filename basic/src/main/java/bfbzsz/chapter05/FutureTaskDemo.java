283dbebbf1d1800f0c6bd89cade02a44f3fc32cf
john.huang <john.huang@cloudwise.com>
2023-02-20 09:33:04 +0800

    exercise

---
+++ basic/src/main/java/bfbzsz/chapter05/FutureTaskDemo.java
@@ -0,0 +1,12 @@
+package bfbzsz.chapter05;
+
+import java.util.concurrent.FutureTask;
+
+public class FutureTaskDemo {
+
+//    private final FutureTask futureTask = new FutureTask();
+
+    public static void main(String[] args) {
+
+    }
+}
