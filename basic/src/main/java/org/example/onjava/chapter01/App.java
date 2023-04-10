41c7b66ad4ed054d15f39b462eb17a2600e713a2
john.huang <john.huang@cloudwise.com>
2023-04-10 17:13:16 +0800

    复习对象的基本概念1

---
+++ basic/src/main/java/org/example/onjava/chapter01/App.java
@@ -0,0 +1,22 @@
+package org.example.onjava.chapter01;
+
+/**   
+ * Copyright © 2023 eSunny Info. Tech Ltd. All rights reserved.
+ * 
+ * @Package: org.example.onjava.chapter01 
+ * @author: Administrator   
+ * @date: 2023年4月10日 下午5:10:18
+ * @desc:
+ */
+public class App {
+	
+	public static void main(String[] args) {
+		
+		//1.编程语言的解释
+		//2.对象的五大特点
+		//3.对象具有接口
+		//4.对象可以提供服务
+		//5.对象需要面向客户程序员隐藏实现
+	}
+
+}
